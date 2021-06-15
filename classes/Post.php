0e2a50c093b2fe394fe9aec88cee6be68db53493
Анна Гошкодер <85807541+AnnH1994@users.noreply.github.com>
2021-06-15 21:04:02 +0300

    create class Posts.php

--- classes/Post.php
+++ classes/Post.php
@@ -3,5 +3,30 @@
 
 class Post
 {
+    public $datebase;
+    public  function __construct()
+    {
+        $this->datebase = new mysqli(DB_HOST, DB_USER, DB_PASS, DB_NAME);
+    }
+    public function getAllPosts(){
+        $sql = "SELECT*FROM news;";
+        $result = $this->datebase->query($sql);
+        if($result){
+            $posts=[];
+            while ($post = $result->fetch_assoc()){
+                $posts[]=$post;
+            }
+            return $posts;
+        }
+        return null;
+    }
+    public  function addPost($name, $textPost, $date, $user_id){
+        $sql= "INSERT INTO news (name, text, date, user_id) VALUES ('$name','$textPost',$date, $user_id)";
+        $this->datebase->query($sql);
+    }
+    public  function  delPost ($id){
+        $sql= "DELETE  FROM news WHERE news.id = $id";
+        $this->datebase->query($sql);
+    }
 
 }
