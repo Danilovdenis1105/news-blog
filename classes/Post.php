d28d8b94c79eded2142162e371b32a7497e03d03
Danilov Denis <danilovdenis479@gmail.com>
2021-06-16 18:57:57 +0300

    added new files, methods, class Database

--- classes/Post.php
+++ classes/Post.php
@@ -1,7 +1,18 @@
 <?php
 
 
-class Post
+class Post extends Database
 {
+    public function addPost($name, $description, $user_id){
+        $sql = "INSERT INTO news (name, description,user_id) values ('$name','$description','$user_id')";
+        $this->dbConnect->query($sql);
+    }
+    public function deletePost($id){
+        $sql = "DELETE FROM news WHERE `news`.`id` = $id limit 1;";
+        $this->dbConnect->query($sql);
+    }
 
+    public function editPost($id, $name, $description){
+        $sql = "UPDATE `news` SET `name` = '$name', `description` = '$description' WHERE `news`.`id` = '$id';";
+    }
 }
--- classes/User.php
+++ classes/User.php
@@ -1,7 +1,16 @@
 <?php
 
 
-class User
+class User extends Database
 {
+    public function addUser($login, $password){
+        $password = password_hash($password, PASSWORD_DEFAULT);
+        $sql = "INSERT INTO users (login, password) values ('$login','$password')";
+        $this->dbConnect->query($sql);
+    }
 
+    public function deleteUser($id){
+        $sql = "DELETE FROM users WHERE `users`.`id` = $id limit 1;";
+        $this->dbConnect->query($sql);
+    }
 }
