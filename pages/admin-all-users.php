948dd57ddc8cff3b3ce1d944c35efd47ec3796f8
Ilina <orel.Ilona@gmail.com>
2021-06-17 01:52:31 +0300

    fixed admin-all-users.php

--- pages/admin-all-users.php
+++ pages/admin-all-users.php
@@ -1,15 +1,3 @@
-<!doctype html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" href="../css/admin-all-users.css">
-    <title>Document</title>
-</head>
-<body>
-
 <table>
     <caption>Users</caption>
     <tr>
@@ -17,15 +5,11 @@
         <th>Login</th>
         <th></th>
     </tr>
-    <!--    --><?php //if(count($this->users)>0):?>
-    <!--        --><?php //foreach ($this->users as $user):?>
+        <?php if(count($this->users)>0):?>
+            <?php foreach ($this->users as $user):?>
     <tr>
-        <td>1
-            <!--            --><?//=$user['id']?>
-        </td>
-        <td>2
-            <!--            --><?//=$user['login']?>
-        </td>
+        <td><?=$user['id']?></td>
+        <td><?=$user['login']?></td>
         <td class="delete">
             <form action="/admin.php" method="post">
                 <input type="submit" value="del"/>
@@ -33,8 +17,8 @@
             </form>
         </td>
     </tr>
-    <!--    --><?php //endforeach;?>
-    <!--    --><?php //endif;?>
+        <?php endforeach;?>
+        <?php endif;?>
 </table>
 <div>
     <form action="/admin.php" method="post" >
